81a960503726ada8d925068f7aa939782c4fad80
Dr.Sc.KAWAMOTO,Takuji <DrScKAWAMOTO@users.noreply.github.com>
2014-09-19 18:44:24 +0900

    ファイル類のパスを data に変更。

--- deploy.bat
+++ deploy.bat
@@ -1,5 +1,5 @@
 set QTBIN=C:\Qt\Qt5.3.1\5.3\mingw482_32\bin
-set DATA=packages\FullereneViewer\data
+set DATA=data
 set RELEASE=..\FullereneViewer\src\build-FullereneViewer-Desktop_Qt_5_3_MinGW_32bit-Release\release
 set PATH=%QTBIN%;%PATH%
 copy %RELEASE%\FullereneViewer.exe %DATA%
