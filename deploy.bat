4dc7dc31c5e9322080042669779a0b0995a821a6
Dr.Sc.KAWAMOTO,Takuji <DrScKAWAMOTO@users.noreply.github.com>
2014-12-31 17:55:05 +0900

    FullereneViewer V1.4.2 対応

--- deploy.bat
+++ deploy.bat
@@ -1,6 +1,6 @@
-set QTBIN=C:\Qt\Qt5.3.1\5.3\mingw482_32\bin
+set QTBIN=C:\Qt\Qt5.4.0\5.4\mingw491_32\bin
 set DATA=data
-set RELEASE=..\FullereneViewer\src\build-FullereneViewer-Desktop_Qt_5_3_MinGW_32bit-Release\release
+set RELEASE=..\FullereneViewer\src\build-FullereneViewer-Desktop_Qt_5_4_0_MinGW_32bit-Release\release
 set PATH=%QTBIN%;%PATH%
 copy %RELEASE%\FullereneViewer.exe %DATA%
 windeployqt.exe %DATA%\FullereneViewer.exe
